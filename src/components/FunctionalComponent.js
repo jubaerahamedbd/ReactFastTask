921f609c9f30691820faa917482bb155ac04f6c7
jubaerahamedbd <jubaerahamed@gmail.com>
2021-11-08 12:00:48 +0600

    Arrow Function

--- src/components/FunctionalComponent.js
+++ src/components/FunctionalComponent.js
@@ -1,13 +1,13 @@
 import React from "react";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-function FunctionalComponent(props){
-    function DoThis(e){
+const FunctionalComponent = props => {
+    const DoThis = e => {
         alert(e)
     }
-    return(
+    return (
         <div>
-            <button className="btn btn-primary" onClick={DoThis.bind(this,"I am from Function Parameter")}>Click Me From Function</button>
+            <button className="btn btn-danger" onClick={DoThis.bind(this, "I am from Function Parameter")}>Click Me From Function</button>
             <h3>Name: {props.name} and Age: {props.age}</h3>
         </div>
     )
