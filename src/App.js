9b79a0a2d4c3dbf0b637c7716d1197b9ac7dedba
jubaerahamedbd <jubaerahamed@gmail.com>
2021-11-08 11:32:25 +0600

    Props Handle With FunctionalComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css';
 import ClassComponent from './components/ClassComponent';
+import FunctionalComponent from './components/FunctionalComponent';
 function App() {
   return (
     <div className="App">
-      <ClassComponent/>
+      <ClassComponent />
+      <FunctionalComponent name="Jubaer" age="25"/>
+      <FunctionalComponent name="Shakil" age="22"/>
+      <FunctionalComponent name="Ashraf" age="19"/>
     </div>
   )
 
---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function FunctionalComponent(props){
+    return(
+        <h3>Name: {props.name} and Age: {props.age}</h3>
+    )
+}
+
+export default FunctionalComponent;
