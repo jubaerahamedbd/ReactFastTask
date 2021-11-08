6d19dddc52c08dbd45e4cb34db292d92b3ac4f11
jubaerahamedbd <jubaerahamed@gmail.com>
2021-11-08 11:48:45 +0600

    Passing Argument

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 class ClassComponent extends Component {
 
-    DoThis(){
-        alert("Button is Clicked From Class")
+    DoThis(e){
+        alert(e)
     }
     render() {
         return (
             <div>
-                <button onClick={this.DoThis}>Click Me From Class</button>
+                <button onClick={this.DoThis.bind(this,"I am from Class Parameter")}>Click Me From Class</button>
                 <h4>City Name: {this.props.cityname}</h4>
             </div>
         )
--- src/components/FunctionalComponent.js
+++ src/components/FunctionalComponent.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function FunctionalComponent(props){
-    function DoThis(){
-        alert("Button is Clicked From Function")
+    function DoThis(e){
+        alert(e)
     }
     return(
         <div>
-            <button onClick={DoThis}>Click Me From Function</button>
+            <button onClick={DoThis.bind(this,"I am from Function Parameter")}>Click Me From Function</button>
             <h3>Name: {props.name} and Age: {props.age}</h3>
         </div>
     )
